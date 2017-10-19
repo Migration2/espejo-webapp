c718824b4739ec184bc8f7e5ab22abce8cd2d4eb
MANUEL GARIZAO <MANUELFP@GMAIL.COM>
2017-10-18 19:22:00 -0500

    - FIX problema redireccion una ves se loguea el usuario con sus credenciales. - Se crea el controlador que regresa los roles.

--- src/main/java/net/devcodes/webclient/controller/PrincipalRest.java
+++ src/main/java/net/devcodes/webclient/controller/PrincipalRest.java
@@ -2,6 +2,9 @@ package net.devcodes.webclient.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,19 +18,30 @@ import java.util.Map;
  */
 
 @RestController
-@RequestMapping("/rest/init")
+@RequestMapping("/init")
 public class PrincipalRest {
 
-    @GetMapping("/me")
-    public ResponseEntity<Map<String, String>> getPrincipal(Principal principal) {
-        Map<String, String> map = new LinkedHashMap<>();
+  @GetMapping("/me")
+  public ResponseEntity<Map<String, String>> getPrincipal(Principal principal) {
+    Map<String, String> map = new LinkedHashMap<>();
 
-
-        if (principal != null) {
-            map.put("name", principal.getName());
-        } else {
-            map.put("name", "anonimo");
-        }
-        return new ResponseEntity<>(map, HttpStatus.OK);
+    if (principal != null) {
+      map.put("name", principal.getName());
+    } else {
+      map.put("name", "anonimo");
     }
+    return new ResponseEntity<>(map, HttpStatus.OK);
+  }
+
+  @GetMapping("/me/roles")
+  public ResponseEntity<UserDetails> me(Principal principal) {
+    if (principal != null) {
+      Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+      System.out.println(loggedInUser.getAuthorities());
+      return new ResponseEntity(loggedInUser.getAuthorities(), HttpStatus.OK);
+    } else {
+      return new ResponseEntity(HttpStatus.OK);
+    }
+
+  }
 }
